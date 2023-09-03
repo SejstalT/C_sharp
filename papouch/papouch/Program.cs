02ace29e07c275fab6eade8157472366079b9d1c
SejstalT <114529364+SejstalT@users.noreply.github.com>
2023-09-03 09:57:45 +0200

    Add files via upload

---
+++ papouch/papouch/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ahoj jsem papoušek a umím vše opakovat!");
+Console.WriteLine("napiš text k zopakování:");
+
+string vstup;
+vstup = Console.ReadLine();
+
+Console.WriteLine(vstup + " " + vstup + " !");
+Console.ReadKey();
