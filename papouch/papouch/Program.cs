c9ac93f04fba6caa919cfb34a7f06b414395534b
SejstalT <114529364+SejstalT@users.noreply.github.com>
2023-09-03 10:00:39 +0200

    Delete papouch directory

--- papouch/papouch/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Ahoj jsem papoušek a umím vše opakovat!");
-Console.WriteLine("napiš text k zopakování:");
-
-string vstup;
-vstup = Console.ReadLine();
-
-Console.WriteLine(vstup + " " + vstup + " !");
-Console.ReadKey();
